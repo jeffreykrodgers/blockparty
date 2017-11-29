fc2a8720b4fe9967fd6837304ae272c084f5bcf7
Unknown <jeffrey.rodgers@outlook.com>
2017-11-28 21:31:03 -0500

    Created basic widget structure and added countdown timer

--- client/imports/app/admin/components/countdown/countdown.component.ts
+++ client/imports/app/admin/components/countdown/countdown.component.ts
@@ -3,6 +3,9 @@ import template from "./countdown.component.html";
 import style from "./countdown.component.scss";
 import {ModalService} from "../modals/modals.service";
 
+import * as moment from 'moment';
+import {WeddingService} from "../../../common/services/wedding.service";
+
 @Component({
     selector: "countdown",
     host: {
@@ -14,12 +17,44 @@ import {ModalService} from "../modals/modals.service";
 
 export class admin_CountdownComponent implements OnInit {
     @Input() slug: string;
+    weddingData: any;
+    countdown: any;
+    moment: any;
 
-    constructor(private _modalService: ModalService) {
-
+    constructor(private _modalService: ModalService,
+                private _weddingService: WeddingService) {
+        this.countdown = {};
+        this.moment = moment;
     }
 
     ngOnInit() {
+        this.weddingData = this._weddingService.getWedding({}).zone();
+        this.weddingData.subscribe(wedding => {
+            this.weddingData = wedding[0];
+            this.getCountdown();
+        });
 
+        let self = this;
+
+        setInterval(function () {
+            self.getCountdown();
+        }, 10000);
+    };
+
+    private getCountdown() {
+        let _weddingDate: any = moment(this.weddingData.date);
+        let _currentDate: any = moment();
+        let _diffTime: any = _weddingDate - _currentDate;
+        let _duration: any = moment.duration(_diffTime);
+
+        if (_diffTime > 0) {
+            //TODO Fix to use moment.diff instead of duration
+            this.countdown = {
+                months: _duration._data.months,
+                days: _duration._data.days + 1,
+                hours: _duration._data.hours,
+                minutes: _duration._data.minutes,
+            }
+        }
     }
 }
