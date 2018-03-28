1446aeff00490fe5ca06569caafa68c352d7c128
Unknown <jeffrey.rodgers@outlook.com>
2018-03-28 17:53:41 -0400

    Added ability for guests to change their names

--- client/imports/app/content/components/guest/guest.ts
+++ client/imports/app/content/components/guest/guest.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {Component, OnInit, Output, EventEmitter} from "@angular/core";
 import template from "./guest.html";
 import style from "../../style/themes/default/guest.scss";
 import {RsvpService} from "../../services/rsvp.service";
@@ -9,15 +9,18 @@ declare let $: any;
 @Component({
     selector: "guest",
     template,
-    styles: [ style ],
+    styles: [style],
 })
 
 export class GuestComponent implements OnInit {
     rsvpData: any;
     links: object[];
+    editing: boolean;
+    beingEdited: any;
 
     constructor(private _rsvpService: RsvpService,
                 private _router: Router) {
+
     }
 
     ngOnInit() {
@@ -35,15 +38,24 @@ export class GuestComponent implements OnInit {
         $('.ui.checkbox').checkbox();
     };
 
+    editName(guest) {
+        this.editing = true;
+        this.beingEdited = guest._id;
+    }
+
+    saveGuest() {
+        this.editing = false;
+        this.beingEdited = false;
+    }
+
     setGuests() {
         this.links = this.rsvpData.links.filter(
-            (link:any) => link.name === 'Meals');
+            (link: any) => link.name === 'Meals');
 
         if (this.links.length === 0)
-            this.rsvpData.links.push( {name: 'Meals', slug: '/rsvp/meals'});
+            this.rsvpData.links.push({name: 'Meals', slug: '/rsvp/meals'});
 
         this._rsvpService.setRsvpData(this.rsvpData, true);
         this._router.navigate(['/rsvp/meals']);
-
     }
 }
